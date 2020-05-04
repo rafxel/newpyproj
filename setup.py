7722e721e9e824ace31bc6452c183cef36e92b2f
Rafael Leite Paulo <ju558@trf1.jus.br>
2020-05-04 01:11:09 -0300

    ver bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="newpyproj",
-    version="0.0.1",
+    version="0.0.2",
     author="Rafael Leite Paulo",
     author_email="rafael@leite.org",
     description="Automatically creates a new python project folder",
