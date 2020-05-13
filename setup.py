41f64725999dd13666ac4c1defb8db8f44d4ef36
««Råƒæ£»» L. P <rafae@TJRR3004017.TJRR>
2020-05-13 18:38:32 -0400

    .gitignore file correction

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="newpyproj",
-    version="0.0.3",
+    version="0.0.4",
     author="Rafael Leite Paulo",
     author_email="rafael@leite.org",
     description="Automatically creates a new python project folder",
